66873db72d8ebe7bc286e32cb91aa4f43f6e983b
中村祐馬 <yuma.nakamura.0309@gmail.com>
2024-03-01 00:26:50 +0900

    リスト10.3までの動作を確認

--- Manabu/Laravel_sample_Yuma/fastapi-book-example/api/schemas/task.py
+++ Manabu/Laravel_sample_Yuma/fastapi-book-example/api/schemas/task.py
@@ -9,6 +9,13 @@ class TaskCreate(TaskBase):
     pass
 
 
+class TaskCreateResponse(TaskCreate):
+    id: int
+
+    class Config:
+        orm_mode = True
+
+
 class Task(TaskBase):
     id: int
     done: bool = Field(False, description="完了フラグ")
