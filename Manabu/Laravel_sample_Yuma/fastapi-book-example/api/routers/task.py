81e7740cd12726cf9e0164dba2845cbdca24be5f
中村祐馬 <yuma.nakamura.0309@gmail.com>
2024-03-12 01:13:56 +0900

    リスト12.3までの動作を確認

--- Manabu/Laravel_sample_Yuma/fastapi-book-example/api/routers/task.py
+++ Manabu/Laravel_sample_Yuma/fastapi-book-example/api/routers/task.py
@@ -15,8 +15,8 @@ async def list_tasks():
 
 
 @router.post("/tasks", response_model=task_schema.TaskCreateResponse)
-async def create_task(task_body: task_schema.TaskCreate):
-    return task_schema.TaskCreateResponse(id=1, **task_body.dict())
+async def create_task(task_body: task_schema.TaskCreate, db: Session = Depends(get_db)):
+    return task_crud.create_task(db, task_body)
 
 
 @router.put("/tasks/{task_id}", response_model=task_schema.TaskCreateResponse)
