77d965da229cbf9917c6d3f03099321e3b66c502
中村祐馬 <yuma.nakamura.0309@gmail.com>
2024-03-29 00:09:53 +0900

    リスト13.8までの動作を確認

--- Manabu/Laravel_sample_Yuma/fastapi-book-example/api/cruds/task.py
+++ Manabu/Laravel_sample_Yuma/fastapi-book-example/api/cruds/task.py
@@ -1,4 +1,3 @@
-from sqlalchemy.orm import Session
 from sqlalchemy import select
 from sqlalchemy.engine import Result
 from sqlalchemy.ext.asyncio import AsyncSession
@@ -46,6 +45,6 @@ async def update_task(
     return original
 
 
-def delete_task(db: Session, original: task_model.Task) -> None:
-    db.delete(original)
-    db.commit()
+async def delete_task(db: AsyncSession, original: task_model.Task) -> None:
+    await db.delete(original)
+    await db.commit()
