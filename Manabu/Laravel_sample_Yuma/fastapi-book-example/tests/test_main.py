85a8373fb9fd08bfcfc16217bd3cbfe4de028d4e
中村祐馬 <yuma.nakamura.0309@gmail.com>
2024-04-16 00:19:04 +0900

    リスト14.16までの動作を確認

--- Manabu/Laravel_sample_Yuma/fastapi-book-example/tests/test_main.py
+++ Manabu/Laravel_sample_Yuma/fastapi-book-example/tests/test_main.py
@@ -76,14 +76,17 @@ async def test_done_flag(async_client):
 
 
 @pytest.mark.asyncio
-async def test_due_date(async_client):
-    input_list = ["2024-12-01", "2024-12-32", "2024/12/01", "2024-1201"]
-    expectation_list = [
-        starlette.status.HTTP_200_OK,
-        starlette.status.HTTP_422_UNPROCESSABLE_ENTITY,
-        starlette.status.HTTP_422_UNPROCESSABLE_ENTITY,
-        starlette.status.HTTP_422_UNPROCESSABLE_ENTITY,
-    ]
-    for input_param, expectation in zip(input_list, expectation_list):
-        response = await async_client.post("/tasks", json={"title": "テストタスク", "due_date": input_param})
-        assert response.status_code == expectation
+@pytest.mark.parametrize(
+    "input_param, expectation",
+    [
+        ("2024-12-01", starlette.status.HTTP_200_OK),
+        ("2024-12-32", starlette.status.HTTP_422_UNPROCESSABLE_ENTITY),
+        ("2024/12/01", starlette.status.HTTP_422_UNPROCESSABLE_ENTITY),
+        ("2024-1201", starlette.status.HTTP_422_UNPROCESSABLE_ENTITY),
+    ],
+)
+async def test_due_date(input_param, expectation, async_client):
+    response = await async_client.post(
+        "/tasks", json={"title": "テストタスク", "due_date": input_param}
+    )
+    assert response.status_code == expectation
